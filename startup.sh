88c909d849e1f39ceb748ea5db463e8395a697fe
anaszxi <anasap13@hotmail.com>
2024-12-09 03:21:10 +0300

    Fix Azure Blob Storage configuration and add model_utils.py

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Download model from Azure Blob Storage
+python manage.py shell -c "from core.livenews.model_utils import download_model_from_blob; download_model_from_blob()"
+
 # Make migrations
 python manage.py makemigrations
 python manage.py migrate
