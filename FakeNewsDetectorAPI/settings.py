321a12536d547a90ef39c502eb3c8399e4bbe33a
Anas Talal <129590796+anaszxi@users.noreply.github.com>
2024-12-10 23:04:03 +0300

    Update settings.py

--- FakeNewsDetectorAPI/settings.py
+++ FakeNewsDetectorAPI/settings.py
@@ -8,11 +8,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-secret-key-here'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
