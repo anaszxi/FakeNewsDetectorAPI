dace34cb156d85e274686df2ceefd3cb02eb3c55
Anas Talal <129590796+anaszxi@users.noreply.github.com>
2024-12-11 15:37:13 +0300

    Update settings.py

--- FakeNewsDetectorAPI/settings.py
+++ FakeNewsDetectorAPI/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['fake-news-detector-services.azurewebsites.net',localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['fake-news-detector-services.azurewebsites.net','localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
