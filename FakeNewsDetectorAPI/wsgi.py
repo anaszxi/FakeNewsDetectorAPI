942c308af840f2325d483c9a805f9f663ef97e8f
Anas Talal <129590796+anaszxi@users.noreply.github.com>
2024-12-13 12:22:46 +0000

    upload 5

--- FakeNewsDetectorAPI/wsgi.py
+++ FakeNewsDetectorAPI/wsgi.py
@@ -3,7 +3,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'core.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'core.settings'
+settings_module = 'FakeNewsDetectorAPI.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'FakeNewsDetectorAPI.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 print("Before get_wsgi_application()")
 application = get_wsgi_application()
