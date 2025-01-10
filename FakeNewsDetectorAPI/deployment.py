a4847d8ed95d9299e2495a8c65e8cc95df692361
Anas Talal <129590796+anaszxi@users.noreply.github.com>
2025-01-10 23:37:27 +0000

    update22

--- FakeNewsDetectorAPI/deployment.py
+++ FakeNewsDetectorAPI/deployment.py
@@ -43,7 +43,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'ssl': {
-                'ca': os.path.join(BASE_DIR, 'certs', 'cacert.pem'),
+                'ca': os.path.join(BASE_DIR, 'certs', 'DigiCertGlobalRootCA.crt.pem'),
                 'ssl_disabled': False,  # Enforce SSL connection
             }
         },
